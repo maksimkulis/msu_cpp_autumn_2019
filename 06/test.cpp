1675dfc98d876edb10160ebbd65d323e7153261f
Maksim Kulis <maksimkulis@yandex.ru>
2019-11-17 23:29:21 +0300

    fix

--- 06/test.cpp
+++ 06/test.cpp
@@ -4,14 +4,14 @@
 
 #include "format.h"
 
-#define check_err(str, ...) {err = false; try {format(str, __VA_ARGS__);} catch(std::runtime_error) {err = true;} assert(err);}
+#define check_err(str, ...) {bool err = false; try {format(str, __VA_ARGS__);} catch(std::runtime_error) {err = true;} assert(err);}
 int main()
 {
     assert(!format("{1}opa chirik {0} {1}", 5, 10).compare("10opa chirik 5 10"));
     assert(!format("{0}{1}{2}{4}", false, true, 2, 3, "444").compare("012444"));
     assert(!format("pornhub.com").compare("pornhub.com"));
     assert(!format("{0}", "{0}").compare("{0}"));
-    bool err = false;
+    
     check_err("ras dva{4r}", 5);
     check_err("Chill bez krinja{}", 0);
     check_err("okay boomer}", 2);
